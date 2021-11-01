24d7ffb977a5706d1c0f94d8f4fd634a212ecd03
Praise-Joel <91261675+Praise-Joel@users.noreply.github.com>
2021-11-01 05:27:19 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image,SafeAreaView } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+export default function App() {
+  return(
+    <WelcomeScreen/>
+  );
+ 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00bfff',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+   
+  },
+});
